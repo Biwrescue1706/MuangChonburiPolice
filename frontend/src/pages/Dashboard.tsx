f28046fb25169c869efa60b61a5ff514a8ea6ba0
BiwBong1706 <bewrockgame1@gmail.com>
2026-02-26 21:04:02 +0700

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,20 +1,36 @@
+import Nav from "../Nav";
 import { useAuth } from "../context/AuthContext";
 
 export default function Dashboard() {
-  const { admin, logout } =
-    useAuth();
+  const { admin } = useAuth();
 
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <>
+      {/* Navbar */}
+      <Nav />
 
-      <p>
-        Welcome {admin?.username}
-      </p>
+      {/* Content */}
+      <div className="container mt-4">
 
-      <button onClick={logout}>
-        Logout
-      </button>
-    </div>
+        <div className="card shadow-sm border-0 p-4">
+          <h3 className="fw-bold mb-3">
+            Dashboard
+          </h3>
+
+          <p className="mb-1">
+            ยินดีต้อนรับ
+          </p>
+
+          <h5 className="text-primary">
+            {admin?.name}
+          </h5>
+
+          <small className="text-muted">
+            ตำแหน่ง : {admin?.position}
+          </small>
+        </div>
+
+      </div>
+    </>
   );
 }
