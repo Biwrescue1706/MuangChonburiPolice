b9da7fc12e00938b16d2970982f15fc405ce61a0
BiwBong1706 <bewrockgame1@gmail.com>
2026-02-27 01:14:43 +0700

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// ===== HIDE CONSOLE IN PRODUCTION =====
+if (import.meta.env.PROD) {
+  console.clear();
+
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
 
 ReactDOM.createRoot(
   document.getElementById("root")!
 ).render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
