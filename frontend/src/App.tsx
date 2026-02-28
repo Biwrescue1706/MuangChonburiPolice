85805f1d9cadca64703c2da1f9b1567a311f4730
BiwBong1706 <bewrockgame1@gmail.com>
2026-02-28 20:52:16 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,6 @@ import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import GuestRoute from "./routes/GuestRoute";
 
-
-
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
@@ -14,6 +12,7 @@ import ForgotCheck from "./pages/Password/ForgotCheck";
 import ResetPassword from "./pages/Password/ResetPassword";
 import CreateAdmin from "./pages/CreateAdmin";
 import NotFound from "./pages/NotFound";
+import PersonPage from "./pages/PersonPage";
 
 
 function App() {
@@ -46,7 +45,7 @@ function App() {
             <Route path="/security" element={<Security />} />
             <Route path="/admin/create" element={<CreateAdmin />} />
           </Route>
-
+            <Route path="/person" element={<PersonPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </AuthProvider>
