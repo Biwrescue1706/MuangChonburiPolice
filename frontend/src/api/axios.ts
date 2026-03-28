daa71a1d77fafbbd5548d4ad1dc512438eb940e0
BiwBong1706 <bewrockgame1@gmail.com>
2026-03-29 00:30:23 +0700

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:10000/api",
-  // baseURL: "https://hub-muangchonburi.smartdorm-biwboong.shop/api",
+  //baseURL: "http://localhost:10000/api",
+  baseURL: "https://hub-muangchonburi.smartdorm-biwboong.shop/api",
   withCredentials: true, // ✅ ใช้ cookie JWT
 });
 
