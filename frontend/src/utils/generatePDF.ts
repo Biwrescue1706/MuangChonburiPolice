b8ae247648ec941f928b6a19fc12914312d95cbc
BiwBong1706 <bewrockgame1@gmail.com>
2026-04-10 11:15:39 +0700

    v

--- frontend/src/utils/generatePDF.ts
+++ frontend/src/utils/generatePDF.ts
@@ -98,21 +98,21 @@ export const generatePDF = async (p: any) => {
   draw(page2, p.mother, 190, 30);
   draw(page2, p.spouse, 230, 60);
 
-  draw(page2, p.rank , 180 , 80 );
-  draw(page2, p.fullNameOrg , 200 , 90 );
-  draw(page2, p.position , 200 , 100 );
+  draw(page2, p.rank , 180 , 100 );
+  draw(page2, p.fullNameOrg , 200 , 120 );
+  draw(page2, p.position , 200 , 150 );
 
   // ===== ใบเสร็จ =====
   draw(page2, p.receiptBookNo, 700, -60);
   draw(page2, p.receiptNo, 950, -60);
-  draw(page2, p.receiptDate, 700, -10);
+  draw(page2, p.receiptDate, 700, -20);
 
-  draw(page2, p.money, 700, 35);
-  draw(page2, p.moneyText, 900, 35);
+  draw(page2, p.money, 700, 25);
+  draw(page2, p.moneyText, 900, 25);
 
-  draw(page2, p.organizationName, 700, 50);
-  draw(page2, p.rank, 850, 40);
-  draw(page2, p.fullNameOrg, 900, 40);
+  draw(page2, p.organizationName, 700, 55);
+  draw(page2, p.rank, 850, 120);
+  draw(page2, p.fullNameOrg, 900, 130);
 
   const pdfBytes = await pdfDoc.save();
 
