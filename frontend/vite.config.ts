22036decdba1b465b4de2a06ff9a9e9de09e0c3a
Biwbong <bewrockgame1@gmail.com>
2026-08-13 18:53:47 +0700

    1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "ระบบบันทึกรายรับรายจ่าย",
+        short_name: "รายรับรายจ่าย",
+        description: "ระบบบันทึกรายรับรายจ่าย",
+
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+
+        theme_color: "#4CAF50",
+        background_color: "#ffffff",
+
+        icons: [
+          {
+            src: "/assets/muangchonburi.webp",
+            sizes: "192x192",
+            type: "image/webp",
+          },
+          {
+            src: "/assets/muangchonburi.webp",
+            sizes: "512x512",
+            type: "image/webp",
+          },
+        ],
+      },
+    }),
+  ],
+});
